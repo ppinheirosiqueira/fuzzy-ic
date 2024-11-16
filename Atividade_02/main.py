4e47debd7facef804503d22196db4cacc8bee53f
Pedro Pinheiro <49202702+ppinheirosiqueira@users.noreply.github.com>
2024-11-16 10:53:23 -0300

    Fazendo da forma correta

--- Atividade_02/main.py
+++ Atividade_02/main.py
@@ -16,6 +16,9 @@ def pertinencia_tri(x,a,b,c):
 def pertinencia_gau(x, c, sigma):
     return math.e**((-(x-c)**2)/(2*sigma**2))
 
+def pertinencia_cauchy(x,a,c):
+    return 1/(1 + ((x-c)/a)**2)
+
 def criar_funcoes_pertinencia(diferenca, numero_de_funcoes):
 
     funcoes = []
@@ -31,6 +34,15 @@ def criar_funcoes_pertinencia(diferenca, numero_de_funcoes):
 
     return funcoes
 
+# def criar_funcoes_pertinencia(diferenca, numero_de_funcoes):
+#     funcoes = []
+#     numero_de_divisoes = numero_de_funcoes - 1
+#     intervalo = diferenca/numero_de_divisoes
+#     met_int = 0.5*intervalo
+#     for i in range(numero_de_funcoes):
+#         funcoes.append([met_int,intervalo*i])
+#     return funcoes
+
 def calcular_z(x,funcoes_pertinencia,vetor_funcoes_z):
     z = []
     for ponto in x:
@@ -39,8 +51,9 @@ def calcular_z(x,funcoes_pertinencia,vetor_funcoes_z):
         for i in range(len(funcoes_pertinencia)):
             # pertinencia_ponto = pertinencia_tri(ponto,*funcoes_pertinencia[i])
             pertinencia_ponto = pertinencia_gau(ponto,*funcoes_pertinencia[i])
+            # pertinencia_ponto = pertinencia_cauchy(ponto, *funcoes_pertinencia[i])
             somatorio_baixo += pertinencia_ponto
-            somatorio_cima += pertinencia_ponto*vetor_funcoes_z[i][0] + vetor_funcoes_z[i][1]
+            somatorio_cima += pertinencia_ponto*(vetor_funcoes_z[i][0]*ponto + vetor_funcoes_z[i][1])
         z.append(somatorio_cima/somatorio_baixo if somatorio_baixo != 0 else 1)
     return z
 
@@ -54,13 +67,13 @@ def calcular_erro(z, f_x, erros):
     erros.append(erro)
 
 def att_funcoes(funcoes_pertinencia, vetor_funcoes_z, i, j, multiplicador, ganho, geracao_atual):
-    if j > 1: 
-        vetor_funcoes_z[i][j - 2] += multiplicador*ganho
-    else:
-        funcoes_pertinencia[i][j] += multiplicador*ganho
-        if j == 1:
-            if funcoes_pertinencia[i][1] <= 0:
-                funcoes_pertinencia[i][1] = 0.01
+    # if j > 1: 
+    vetor_funcoes_z[i][j] += multiplicador*ganho
+    # else:
+    #     funcoes_pertinencia[i][j] += multiplicador*ganho
+    #     if j == 1:
+    #         if funcoes_pertinencia[i][1] <= 0:
+    #             funcoes_pertinencia[i][1] = 0.01
     return geracao_atual + 1
 
 def check_erro(menor_erro, geracao_atual, erros, z):
@@ -79,13 +92,7 @@ def att_z_e_erro(i, j, x, funcoes_pertinencia, vetor_funcoes_z, f_x, erros, meno
         menor_z, menor_erro, melhor_ger = resultado
     return geracao_atual, menor_z, menor_erro, melhor_ger
 
-def aproximar(numero_funcoes, erro_minimo):
-    # Definir o intervalo de x de 0 a 10 com um número suficiente de pontos
-    x = np.linspace(0, 10, 100)
-
-    # Definir a função f(x) = e^(-x/5) * sin(3x) + 0.5 * sin(x)
-    f_x = np.exp(-x / 5) * np.sin(3 * x) + 0.5 * np.sin(x)
-
+def aproximar(x, f_x, numero_funcoes, erro_minimo):
     funcoes_pertinencia = criar_funcoes_pertinencia(10, numero_funcoes)
     vetor_funcoes_z = [[random.uniform(-1, 1), random.uniform(-1, 1)] for _ in range(numero_funcoes)]
 
@@ -97,7 +104,7 @@ def aproximar(numero_funcoes, erro_minimo):
 
     geracao_atual = 0
     melhor_ger = geracao_atual
-    numero_teste_geracoes = numero_funcoes * (2 + len(funcoes_pertinencia[0]))
+    numero_teste_geracoes = numero_funcoes * 2
     maximo_geracoes = 100*numero_teste_geracoes
 
     z = calcular_z(x, funcoes_pertinencia, vetor_funcoes_z)
@@ -108,38 +115,35 @@ def aproximar(numero_funcoes, erro_minimo):
         menor_z, menor_erro, melhor_ger = resultado
 
     ganho = 1
-    multiplicador = 10
+    multiplicador = 1
 
-    while menor_erro > erro_minimo and geracao_atual - melhor_ger < maximo_geracoes:
-        i = random.randint(0,numero_funcoes - 1)
-        j = random.randint(0, 1 + len(funcoes_pertinencia[0]))
+    while menor_erro > erro_minimo and ganho > 0.01*erro_minimo: #geracao_atual - melhor_ger < maximo_geracoes: 
+        for i in range(numero_funcoes):
+            for j in range(2):
+                geracao_atual, menor_z, menor_erro, melhor_ger = att_z_e_erro(i, j, x, funcoes_pertinencia, vetor_funcoes_z, f_x, erros, menor_erro, geracao_atual, multiplicador, ganho, menor_z, melhor_ger)
 
-        geracao_atual, menor_z, menor_erro, melhor_ger = att_z_e_erro(i, j, x, funcoes_pertinencia, vetor_funcoes_z, f_x, erros, menor_erro, geracao_atual, multiplicador, ganho, menor_z, melhor_ger)
+                while(erros[-2] > erros[-1]):
+                    valores_anteriores_funcoes = copy.deepcopy(funcoes_pertinencia)
+                    valores_anteriores_z = copy.deepcopy(vetor_funcoes_z)
+                    geracao_atual, menor_z, menor_erro, melhor_ger = att_z_e_erro(i, j, x, funcoes_pertinencia, vetor_funcoes_z, f_x, erros, menor_erro, geracao_atual, multiplicador, ganho, menor_z, melhor_ger)
 
-        while ((erros[-2] - erros[-1])/erros[-2] > 0.05*ganho):
-            valores_anteriores_funcoes = copy.deepcopy(funcoes_pertinencia)
-            valores_anteriores_z = copy.deepcopy(vetor_funcoes_z)
-            geracao_atual, menor_z, menor_erro, melhor_ger = att_z_e_erro(i, j, x, funcoes_pertinencia, vetor_funcoes_z, f_x, erros, menor_erro, geracao_atual, multiplicador, ganho, menor_z, melhor_ger)
+                multiplicador *= -1
 
-        multiplicador *= -1
+                funcoes_pertinencia = copy.deepcopy(valores_anteriores_funcoes)
+                vetor_funcoes_z = copy.deepcopy(valores_anteriores_z)
+                
+                geracao_atual, menor_z, menor_erro, melhor_ger = att_z_e_erro(i, j, x, funcoes_pertinencia, vetor_funcoes_z, f_x, erros, menor_erro, geracao_atual, multiplicador, ganho, menor_z, melhor_ger)
 
-        funcoes_pertinencia = copy.deepcopy(valores_anteriores_funcoes)
-        vetor_funcoes_z = copy.deepcopy(valores_anteriores_z)
-        
-        geracao_atual, menor_z, menor_erro, melhor_ger = att_z_e_erro(i, j, x, funcoes_pertinencia, vetor_funcoes_z, f_x, erros, menor_erro, geracao_atual, multiplicador, ganho, menor_z, melhor_ger)
+                while(erros[-2] > erros[-1]):
+                    valores_anteriores_funcoes = copy.deepcopy(funcoes_pertinencia)
+                    valores_anteriores_z = copy.deepcopy(vetor_funcoes_z)
+                    geracao_atual, menor_z, menor_erro, melhor_ger = att_z_e_erro(i, j, x, funcoes_pertinencia, vetor_funcoes_z, f_x, erros, menor_erro, geracao_atual, multiplicador, ganho, menor_z, melhor_ger)
 
-        while ((erros[-2] - erros[-1])/erros[-2] > 0.05*ganho):
-            valores_anteriores_funcoes = copy.deepcopy(funcoes_pertinencia)
-            valores_anteriores_z = copy.deepcopy(vetor_funcoes_z)
-            geracao_atual, menor_z, menor_erro, melhor_ger = att_z_e_erro(i, j, x, funcoes_pertinencia, vetor_funcoes_z, f_x, erros, menor_erro, geracao_atual, multiplicador, ganho, menor_z, melhor_ger)
+                multiplicador *= -1
 
-        multiplicador *= -1
-
-        funcoes_pertinencia = copy.deepcopy(valores_anteriores_funcoes)
-        vetor_funcoes_z = copy.deepcopy(valores_anteriores_z)
-
-        if geracao_atual - melhor_ger > numero_teste_geracoes and ganho > 0.05*erro_minimo:
-            ganho /= 2
+                funcoes_pertinencia = copy.deepcopy(valores_anteriores_funcoes)
+                vetor_funcoes_z = copy.deepcopy(valores_anteriores_z)
+        ganho /= 2
 
     return menor_erro, erros, funcoes_pertinencia, vetor_funcoes_z
 
@@ -180,16 +184,16 @@ zs = {}
 x = np.linspace(0, 10, 100)
 f_x = np.exp(-x / 5) * np.sin(3 * x) + 0.5 * np.sin(x)
 
-for funcao in [10]:
-    for erro in [0.05, 0.01]:
+for funcao in [9]:
+    for erro in [0.1]:
         print(f"{funcao} funções de pertinência e erro mínimo {erro}")
         erro_aux = []
         vet_erro_aux = []
         funcao_aux = []
         vetor_z_aux = []
-        for i in range(1): # Número de testes
+        for i in range(10): # Número de testes
             print(f"Iteração {i}")
-            menor_erro, vet_erros, funcoes_pertinencia, vetor_funcoes_z = aproximar(funcao, erro)
+            menor_erro, vet_erros, funcoes_pertinencia, vetor_funcoes_z = aproximar(x,f_x,funcao, erro)
             erro_aux.append(menor_erro)
             vet_erro_aux.append(vet_erros)
             funcao_aux.append(funcoes_pertinencia)
@@ -201,10 +205,10 @@ for funcao in [10]:
         tamanhos_vetores = [len(vetor) for vetor in vet_erro_aux]
         media_tamanhos = np.mean(tamanhos_vetores)
         desvio_tamanhos = np.std(tamanhos_vetores)
-        print(f'Média dos tamanhos em vet_erro_aux: {media_tamanhos:.4}')
-        print(f'Desvio padrão dos tamanhos em vet_erro_aux: {desvio_tamanhos:.4}')
+        print(f'Média dos tamanhos em vet_erro_aux: {media_tamanhos:.2f}')
+        print(f'Desvio padrão dos tamanhos em vet_erro_aux: {desvio_tamanhos:.2f}')
         indice_menor_erro = erro_aux.index(min(erro_aux))
-        print(f'{media_erro_aux:.4} & {desvio_erro_aux:.4} & {media_tamanhos:6.4} & {desvio_tamanhos:6.4}')
+        print(f'{min(erro_aux):.4} & {media_erro_aux:.4} & {desvio_erro_aux:.4} & {media_tamanhos:.2f} & {desvio_tamanhos:.2f}')
         print(f"Menor erro encontrado {funcao} - {erro}: {min(erro_aux):.4}")
         erros[f"{funcao} funções de pertinência e erro mínimo {erro}"] = vet_erro_aux[indice_menor_erro]
         erros_min[f"{funcao} funções de pertinência e erro mínimo {erro}"] = erro_aux[indice_menor_erro]
